38f4b31460912e0fea4c895335197ff61a8468a7
Klaudiusz Dembler <accounts@kdembler.com>
2023-11-21 19:10:48 +0100

    moved repo

--- runners/distributor.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# docker entrypoint fot distributor node, to allow running with telemetry
-if [[ "$ENABLE_TELEMETRY" = "yes" ]] && [[ $1 = "start" ]]; then
-    node --require @joystream/opentelemetry ./bin/run $*
-else
-    ./bin/run $*
-fi
--- runners/storage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# docker entrypoint fot storage node, to allow running with telemetry
-if [[ "$ENABLE_TELEMETRY" = "yes" ]] && [[ $1 = "server" ]]; then
-    node --require @joystream/opentelemetry ./bin/run $*
-else
-    ./bin/run $*
-fi
